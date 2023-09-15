3e9973d5de2a8e23262e8033d831d883f4855086
Daniel Soares <campossoares.daniel@gmail.com>
2023-09-15 11:44:18 -0300

    php-correction

--- enviar_email.php
+++ enviar_email.php
@@ -1,18 +1,42 @@
+
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST["name"];
-  $email = $_POST["email"];
-  $message = $_POST["message"];
+ini_set("SMTP", "campossoares.daniel@gmail.com");
+ini_set("smtp_port", "587");
+
+if (isset($_POST['email']) && !empty($_POST['email'])) {
+  $msg = '';
+
+  $name = addslashes($_POST["name"]);
+  $email = addslashes($_POST["email"]);
+  $message = addslashes($_POST["message"]);
 
   $to = "campossoares.daniel@gmail.com";
   $subject = "Formulário de Contato";
-  $headers = "From: $name <$email>";
   $body = "Mensagem: $message";
+  $headers = "From:campossoares.daniel@gmail.com"."\r\n".
+              "Reply-To:".$email. "\r\n".
+              "X=Mailer:PHP/".phpversion();
+
+
 
   if (mail($to, $subject, $body, $headers)) {
-    echo "Mensagem enviada com sucesso!";
+    $msg = '<div class="alert alert-success alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Mensagem enviada com sucesso!</strong> 
+            </div>';
+    
   } else {
-    echo "Erro ao enviar a mensagem.";
+    $msg = '<div class="alert alert-danger alert-dismissable">
+                <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                <strong>Erro ao enviar mensagem, tente novamente! </strong> 
+            </div>';
   }
+} else {
+        
+  //mostra mensagem de erro caso algum dos campos esteja vazio
+  $msg = '<div class="alert alert-danger alert-dismissable">
+          <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+          <strong>Preencha o campo de email!! </strong> 
+      </div>';
 }
 ?>
