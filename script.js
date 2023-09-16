40a88b671940376e394a2a6db89294334db8ea69
Daniel Soares <campossoares.daniel@gmail.com>
2023-09-16 03:26:30 -0300

    atualização

--- script.js
+++ script.js
@@ -1,28 +1,42 @@
 // script.js
-const contactForm = document.getElementById('contact-form');
 
-contactForm.addEventListener('submit', function(event) {
-  event.preventDefault();
-
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const message = document.getElementById('message').value;
-
-  const data = {
-    name: name,
-    email: email,
-    message: message
-  };
-
-  axios.post('enviar-email.php', data)
-    .then(function(response) {
-      // Lógica para lidar com a resposta do envio do email
-      console.log(response);
-      // Exibir uma mensagem de sucesso ou redirecionar para outra página, por exemplo
-    })
-    .catch(function(error) {
-      // Lógica para lidar com erros no envio do email
-      console.log(error);
-      // Exibir uma mensagem de erro ou tomar alguma ação adequada
-    });
+$('.nav a[href^="#"]').on('click', function(e) {
+	e.preventDefault();
+	var id = $(this).attr('href'),
+			targetOffset = $(id).offset().top;
+			
+	$('html, body').animate({ 
+		scrollTop: targetOffset - 100
+	}, 500);
 });
+
+
+// const contactForm = document.getElementById('contact-form');
+
+// contactForm.addEventListener('submit', function(event) {
+//   event.preventDefault();
+
+//   const name = document.getElementById('name').value;
+//   const email = document.getElementById('email').value;
+//   const message = document.getElementById('message').value;
+
+//   const data = {
+//     name: name,
+//     email: email,
+//     message: message
+//   };
+
+//   axios.post('enviar-email.php', data)
+//     .then(function(response) {
+//       // Lógica para lidar com a resposta do envio do email
+//       console.log(response);
+//       // Exibir uma mensagem de sucesso ou redirecionar para outra página, por exemplo
+//     })
+//     .catch(function(error) {
+//       // Lógica para lidar com erros no envio do email
+//       console.log(error);
+//       // Exibir uma mensagem de erro ou tomar alguma ação adequada
+//     });
+// });
+
+
